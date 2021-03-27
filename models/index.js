622aa7b7b4e47c32a275e84cf75a8e5891ff1fdb
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-03-27 13:47:24 -0500

    created comment Model/routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Comment = require('./comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -10,4 +11,11 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+module.exports = { User, Post, Comment };
